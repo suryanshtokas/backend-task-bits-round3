b8c70c103ed943ba6a7a8f1fb6dd7c35888d62c2
Suryansh Tokas <suryansh.tokas@gmail.com>
2025-09-28 11:06:18 +0000

    added logic for order quantity

--- order/views.py
+++ order/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 from .models import Order
 from.serializers import OrderSerializer
 from .permissions import IsSeller, IsBuyer
@@ -12,6 +12,13 @@ class OrderCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated, IsBuyer,]
 
     def perform_create(self, serializer):
+        listing = serializer.validated_data['listing']
+
+        if listing.quantity < serializer.validated_data['quantity']:
+            raise ValidationError("Listing doesen't have enough stock")
+        
+        listing.quantity -= serializer.validated_data["quantity"]
+        listing.save()
         serializer.save(buyer=self.request.user)
 
 
